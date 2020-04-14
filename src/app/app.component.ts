7022f3e31be3f5d89f84c20f39da262b7cd38789
Josh Tate <joshtate04@me.com>
2020-04-14 10:13:09 -0400

    Mortgage calculator component class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { calculateMortgageDetails, PeriodType } from './mortgage-calculator/mortgage-calculator';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'rbc-mortgage-calculator';
+
+  constructor() {
+    console.log(calculateMortgageDetails(300000, 60000, 25, 0, PeriodType.Monthly, .02))
+  }
 }
---
+++ src/app/mortgage-calculator/mortgage-calculator.component.ts
@@ -0,0 +1,34 @@
+import { Component } from "@angular/core";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import { MortgageDetails, calculateMortgageDetails, PeriodType } from './mortgage-calculator';
+
+@Component({
+  selector: 'app-mortgage-calculator',
+  templateUrl: './mortgage-calculator.component.html',
+  styleUrls: ['./mortgage-calculator.component.scss']
+})
+export class MortgageCalculatorComponent {
+  mortgageForm: FormGroup;
+  mortgageDetails?: MortgageDetails;
+
+  constructor(private _fb: FormBuilder) {
+    this.mortgageForm = this._fb.group({
+      totalCost: this._fb.control(100000, [Validators.required, Validators.min(1)]),
+      downPayment: this._fb.control(0, [Validators.min(0)]),
+      years: this._fb.control(25, [Validators.min(1)]),
+      months: this._fb.control(0, [Validators.min(0)]),
+      apr: this._fb.control(0, [Validators.min(0)]),
+      periodType: this._fb.control(PeriodType.Monthly, [Validators.required])
+    });
+  }
+
+  calculate(): void {
+    if (this.mortgageForm.invalid) {
+      return;
+    }
+
+    const {totalCost, downPayment, years, months, apr, periodType} = this.mortgageForm.value;
+
+    this.mortgageDetails = calculateMortgageDetails(totalCost, downPayment, years, months, apr, periodType);
+  }
+}
