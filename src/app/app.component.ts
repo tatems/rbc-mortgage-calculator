faa5139ea48c194b42eb61e3175c7ffdeaca3869
Josh Tate <joshtate04@me.com>
2020-04-14 10:50:27 -0400

    Cleaned up app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,8 @@
 import { Component } from '@angular/core';
-import { calculateMortgageDetails, PeriodType } from './mortgage-calculator/mortgage-calculator';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'rbc-mortgage-calculator';
-
-  constructor() {
-    console.log(calculateMortgageDetails(300000, 60000, 25, 0, PeriodType.Monthly, .02))
-  }
-}
+export class AppComponent {}
